a39c2a148eccd0e07502a715105408888aac5747
Miks Latvis <miks.latvis@gmail.com>
2022-03-08 19:19:12 +0900

    Initial Commit

---
+++ Timeline/TimelineApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimelineApp.swift
+//  Timeline
+//
+//  Created by Miks Latvis on 08/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct TimelineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
